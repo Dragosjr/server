6f8d3ab8482a52a505608adbbfe583da8e7a0dfe
Dennis Dragos <dempaaik@hotmail.com>
2016-11-29 20:03:36 +0100

    ss

--- labb1/src/java/bean/MemberBean.java
+++ labb1/src/java/bean/MemberBean.java
@@ -34,6 +34,7 @@ public class MemberBean {
     private UserDAO userdb;
     private List<Member> users;
     private HtmlDataTable skickas;
+    private String dennis;
 
 
     public MemberBean(){
